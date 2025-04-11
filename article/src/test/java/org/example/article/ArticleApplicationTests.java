258222c7ec5bb4f8d6c4938b08333a59358dcf7f
veronikavanivska <veronikavanivska2005@gmail.com>
2025-04-11 20:23:00 +0200

    Set up the begging of architecture, and first dockerfile

---
+++ article/src/test/java/org/example/article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
