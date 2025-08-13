a5131fbc83775e237059909fbc7942a30aa196a4
veronikavanivska <veronikavanivska2005@gmail.com>
2025-08-13 21:59:40 +0200

    Grpc server

---
+++ auth/src/main/java/org/example/auth/server/GrpcServer.java
@@ -0,0 +1,32 @@
+package org.example.auth.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.example.auth.services.AuthService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GrpcServer {
+    private final AuthService authService;
+
+    public GrpcServer(AuthService authService) {
+        this.authService = authService;
+    }
+
+    public void start() {
+        try {
+            Server server = ServerBuilder.
+                    forPort(9090).
+                    addService(authService)
+                    .build();
+
+            server.start();
+            System.out.println("Server started on port" + server.getPort());
+
+            server.awaitTermination();
+        } catch (Exception e) {
+            System.err.println("Error starting server" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
