91879fe14350ac355d75566248a1abc130139ebf
veronikavanivska <veronikavanivska2005@gmail.com>
2025-08-13 21:58:58 +0200

    create repositories

---
+++ auth/src/main/java/org/example/auth/repositories/UserRepository.java
@@ -0,0 +1,23 @@
+package org.example.auth.repositories;
+
+import org.example.auth.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    @Query("""
+        select u from User u
+        left join fetch u.roles
+        where u.email = :email
+    """)
+    Optional<User> findByEmailWithRoles(@Param("email") String email);
+
+   // Long findIdByEmail(String email);
+}
