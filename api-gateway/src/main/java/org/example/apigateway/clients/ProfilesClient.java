35de2fc06b32ffb6fb10e747fc4ebf92f97486c3
veronikavanivska <veronikavanivska2005@gmail.com>
2025-09-02 15:23:05 +0200

    Create client and endpoints for ProfilesService

---
+++ api-gateway/src/main/java/org/example/apigateway/clients/ProfilesClient.java
@@ -0,0 +1,40 @@
+package org.example.apigateway.clients;
+
+import com.example.generated.*;
+import io.grpc.Channel;
+import org.example.apigateway.Client;
+import org.example.apigateway.requests.profiles.UpdateProfileRequest;
+
+import static org.example.apigateway.config.ProfileMapper.toGrpc;
+
+@Client(
+        host = "${profiles.server.host}",
+        port = "${profiles.server.port}"
+)
+public class ProfilesClient {
+
+    private static ProfilesServiceGrpc.ProfilesServiceBlockingStub stub;
+
+    public static GetProfileResponse getProfile(Long userId) {
+        GetProfileRequest request = GetProfileRequest.newBuilder().setUserId(userId).build();
+
+        return stub.getMyProfile(request);
+    }
+
+    public static UpdateMyProfileResponse updateMyProfile(UpdateProfileRequest request, Long userId) {
+        UpdateMyProfileRequest grpcReq = toGrpc(request, userId);
+
+        return stub.updateMyProfile(grpcReq);
+    }
+
+    public static GetProfileResponse seeSomeoneProfile(Long userId) {
+        SeeSomeoneProfileRequest request = SeeSomeoneProfileRequest.newBuilder().setUserId(userId).build();
+        return stub.seeSomeoneProfile(request);
+    }
+
+
+    public static void init(Channel channel) {
+        stub = ProfilesServiceGrpc.newBlockingStub(channel);
+    }
+
+}
---
+++ api-gateway/src/main/java/org/example/apigateway/controllers/ProfilesController.java
@@ -0,0 +1,45 @@
+package org.example.apigateway.controllers;
+
+import org.example.apigateway.clients.ProfilesClient;
+import org.example.apigateway.config.ProfileMapper;
+import org.example.apigateway.config.SecurityConfig;
+import org.example.apigateway.requests.profiles.UpdateProfileRequest;
+import org.example.apigateway.responses.GetProfileResponse;
+import org.example.apigateway.responses.Response;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfilesController {
+
+    @GetMapping("/me")
+    public GetProfileResponse getProfile() {
+        Long userId =  Long.parseLong(SecurityConfig.getCurrentUserId());
+
+        var response = ProfilesClient.getProfile(userId);
+
+        return ProfileMapper.toResponse(response);
+    }
+
+    @PostMapping("/update")
+    public Response<Void> updateProfile(@RequestBody UpdateProfileRequest request) {
+        Long userId =  Long.parseLong(SecurityConfig.getCurrentUserId());
+
+        var response = ProfilesClient.updateMyProfile(request, userId);
+
+        var api = response.getResponse();
+
+        return new Response<>(
+                api.getCode(),
+                api.getMessage()
+        );
+    }
+
+
+    @GetMapping("/someone")
+    public GetProfileResponse getSomeOneProfile(@RequestParam Long userId) {
+        var response = ProfilesClient.seeSomeoneProfile(userId);
+
+        return ProfileMapper.toResponse(response);
+    }
+}
