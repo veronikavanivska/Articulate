976839093f9e622a51805b2e7df23ea2c6fff6c3
veronikavanivska <veronikavanivska2005@gmail.com>
2025-09-18 18:22:52 +0200

    add grpc server

--- article/src/main/java/org/example/article/ArticleApplication.java
+++ article/src/main/java/org/example/article/ArticleApplication.java
@@ -1,13 +1,21 @@
 package org.example.article;
 
+import org.example.article.server.GrpcServer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ArticleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ArticleApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner startGrpcServer(GrpcServer grpcServer) {
+        return args -> grpcServer.start();
     }
 
 }
---
+++ article/src/main/java/org/example/article/server/GrpcServer.java
@@ -0,0 +1,34 @@
+package org.example.article.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.example.article.service.MEiNImportService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GrpcServer {
+    private final MEiNImportService meinImportService;
+
+    public GrpcServer(MEiNImportService meinImportService) {
+        this.meinImportService = meinImportService;
+    }
+
+    public void start() {
+        try {
+            Server server = ServerBuilder.
+                    forPort(9090)
+                    .maxInboundMessageSize(32 * 1024 * 1024)     // client can receive up to 32 MB
+                    .maxInboundMetadataSize(64 * 1024)
+                    .addService(meinImportService)
+                    .build();
+
+            server.start();
+            System.out.println("Server started on port" + server.getPort());
+
+            server.awaitTermination();
+        } catch (Exception e) {
+            System.err.println("Error starting server" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
