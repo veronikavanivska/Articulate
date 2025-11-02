1c5c52b3355c92e72afc8f4b16362548f2702bd3
veronikavanivska <veronikavanivska2005@gmail.com>
2025-11-02 21:27:24 +0100

    Do the Article client impl

--- api-gateway/src/main/java/org/example/apigateway/clients/ArticleClient.java
+++ api-gateway/src/main/java/org/example/apigateway/clients/ArticleClient.java
@@ -1,44 +1,35 @@
 package org.example.apigateway.clients;
 
-import com.example.generated.ImportMEiNReply;
-import com.example.generated.ImportMEiNRequest;
-import com.example.generated.MEiNImportGrpc;
-import com.example.generated.ProfilesServiceGrpc;
-import com.google.protobuf.ByteString;
+import com.example.generated.ArticleServiceGrpc;
+import com.example.generated.ListCyclesResponse;
+import com.example.generated.ListDisciplinesResponse;
+import com.example.generated.ListTypesResponse;
+import com.google.protobuf.Empty;
 import io.grpc.Channel;
 import org.example.apigateway.Client;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Client(host = "${article.server.host}",
         port = "${article.server.port}"
 )
 public class ArticleClient {
 
-    private static MEiNImportGrpc.MEiNImportBlockingStub stub;
+    private static ArticleServiceGrpc.ArticleServiceBlockingStub stub;
 
-    public static ImportMEiNReply importFile(MultipartFile file, String fileName, String label, long importedBy, boolean activateAfter){
-        ByteString fileBytes = null;
-        try {
-            fileBytes = ByteString.copyFrom(file.getBytes());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        ImportMEiNRequest request = ImportMEiNRequest.newBuilder()
-                .setFile(fileBytes)
-                .setFilename(fileName)
-                .setLabel(label == null ? "" : label)
-                .setImportedBy(importedBy)
-                .setActivateAfter(activateAfter)
-                .build();
-
-        return stub.importFile(request);
+    // dla dropdown
+    public ListTypesResponse listPublicationTypes(){
+        return stub.listPublicationTypes(Empty.getDefaultInstance());
     }
+
+    public ListDisciplinesResponse listDisciplines(){
+        return stub.listDisciplines(Empty.getDefaultInstance());
+    }
+
+    public ListCyclesResponse listCycles(){
+        return stub.listEvalCycles(Empty.getDefaultInstance());
+    }
+
 
     public static void init(Channel channel) {
-        stub = MEiNImportGrpc.newBlockingStub(channel);
+        stub = ArticleServiceGrpc.newBlockingStub(channel);
     }
-
 }
