d0cad6e07c8b4a3c5f65d0b56281d575f59b03c7
veronikavanivska <veronikavanivska2005@gmail.com>
2025-11-02 21:43:10 +0100

    Do the Worker article client impl

---
+++ api-gateway/src/main/java/org/example/apigateway/clients/WorkerArticleClient.java
@@ -0,0 +1,131 @@
+package org.example.apigateway.clients;
+
+import com.example.generated.*;
+
+import com.google.protobuf.FieldMask;
+import io.grpc.Channel;
+import org.example.apigateway.Client;
+
+import java.util.List;
+
+@Client(host = "${article.server.host}",
+        port = "${article.server.port}"
+)
+public class WorkerArticleClient {
+    private static WorkerArticleServiceGrpc.WorkerArticleServiceBlockingStub stub;
+
+
+//    rpc DeletePublication(DeletePublicationRequest) returns (ApiResponse);
+
+    public PublicationView createPublication(long userId, long typeId, long disciplineId, String title, String doi,
+                                             String issn, String eissn, String journalTitle, int publicationYear,
+                                             List<String> coauthors){
+        CreatePublicationRequest request = CreatePublicationRequest.newBuilder()
+                .setUserId(userId)
+                .setTypeId(typeId)
+                .setDisciplineId(disciplineId)
+                .setDoi(doi)
+                .setIssn(issn)
+                .setEissn(eissn)
+                .setJournalTitle(journalTitle)
+                .addAllCoauthors(coauthors)
+                .build();
+
+        return stub.createPublication(request);
+    }
+
+    public PublicationView getPublication(long userId, long publicationId){
+        GetPublicationRequest request = GetPublicationRequest.newBuilder()
+                .setId(publicationId)
+                .setUserId(userId)
+                .build();
+
+        return stub.getPublication(request);
+    }
+
+    public ListPublicationsResponse listMyPublications(long userId, long typeId, long disciplineId, long cycleId,
+                                                       int page, int size, String sortBy, String sortDir){
+
+        ListPublicationsRequest request = ListPublicationsRequest.newBuilder()
+                .setUserId(userId)
+                .setTypeId(typeId)
+                .setDisciplineId(disciplineId)
+                .setCycleId(cycleId)
+                .setPage(page)
+                .setSize(size)
+                .setSortBy(sortBy)
+                .setSortDir(sortDir)
+                .build();
+
+        return stub.listMyPublications(request);
+    }
+
+    public PublicationView updatePublication(long id, long userId, Long typeId, Long disciplineId,
+                                             String title, String doi, String issn, String eissn,
+                                             String journalTitle, Integer publicationYear, List<String> replaceCoauthors){
+        UpdatePublicationRequest.Builder req = UpdatePublicationRequest.newBuilder()
+                .setId(id)
+                .setUserId(userId);  // your proto has userId = 2
+
+        FieldMask.Builder mask = FieldMask.newBuilder();
+
+        if (typeId != null) {
+            req.setTypeId(typeId);
+            mask.addPaths("typeId");
+        }
+        if (disciplineId != null) {
+            req.setDisciplineId(disciplineId);
+            mask.addPaths("disciplineId");
+        }
+        if (title != null) {
+            req.setTitle(title);
+            mask.addPaths("title");
+        }
+        if (doi != null) {
+            req.setDoi(doi);
+            mask.addPaths("doi");
+        }
+        if (issn != null) {
+            req.setIssn(issn);
+            mask.addPaths("issn");
+        }
+        if (eissn != null) {
+            req.setEissn(eissn);
+            mask.addPaths("eissn");
+        }
+        if (journalTitle != null) {
+            req.setJournalTitle(journalTitle);
+            mask.addPaths("journalTitle");
+        }
+        if (publicationYear != null) {
+            req.setPublicationYear(publicationYear);
+            mask.addPaths("publicationYear");
+        }
+        if (replaceCoauthors != null) {
+            req.addAllReplaceCoauthors(replaceCoauthors);
+            mask.addPaths("replaceCoauthors");
+        }
+
+
+        if (mask.getPathsCount() == 0) {
+            throw new IllegalArgumentException("No fields to update");
+        }
+
+        req.setUpdateMask(mask.build());
+
+        return stub.updatePublication(req.build());
+    }
+
+    public ApiResponse deletePublication(long id, long userId){
+        DeletePublicationRequest request = DeletePublicationRequest.newBuilder()
+                .setId(id)
+                .setUserId(userId)
+                .build();
+
+        return stub.deletePublication(request);
+    }
+
+    public static void init(Channel channel) {
+        stub = WorkerArticleServiceGrpc.newBlockingStub(channel);
+    }
+}
