f56dc8eda60ec568bfc0b4d5d5e0831718bf8dee
veronikavanivska <veronikavanivska2005@gmail.com>
2025-11-23 21:49:35 +0100

    Add repository

---
+++ article/src/main/java/org/example/article/repositories/MeinMonoVersionRepository.java
@@ -0,0 +1,8 @@
+package org.example.article.repositories;
+
+import org.example.article.entities.MEiN.monographs.MeinMonoVersion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeinMonoVersionRepository extends JpaRepository<MeinMonoVersion, Long> {
+
+}
